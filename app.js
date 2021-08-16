4968821aebf40dc1bfda682bbe94d3dde07fbfe8
DanielRGilles <danryangilles@gmail.com>
2021-08-16 14:35:58 -0700

    added JS functionality and styled page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { countsAsAYes } from './utils.js';
+
 // import functions and grab DOM elements
 const buttonEl = document.getElementById('button');
 const resultsEl = document.getElementById('results');
@@ -14,7 +16,15 @@ buttonEl.addEventListener('click', () => {
 
     if (!wantsToQuiz) return;
 
+    const answer1 = prompt('Is the Aardvark a nocturnal animal?');
+    if (countsAsAYes(answer1)) correctResponses++;
+    const answer2 = prompt('Is the Aardvark closely related to the pig?');
+    if (!countsAsAYes(answer2)) correctResponses++;
+    const answer3 = prompt('Are Aardvarks related to Elephants?');
+    if (countsAsAYes(answer3)) correctResponses++;
 
+    resultsEl.textContent = `Hey ${firstName} ${lastName} you got ${correctResponses} correct out of 3`;
+    resultsEl.style.display = 'inline'; 
 });
   // get user input
   // use user input to update state 
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function countsAsAYes(yeahSure) {
+    return yeahSure.charAt(0).toLowerCase() === 'y';
+}
